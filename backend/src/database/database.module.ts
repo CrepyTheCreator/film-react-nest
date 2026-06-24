4c47761e9c4121bae230481c3a340f2d5643fe16
Misha Zhuravlev <mishami816@gmail.com>
2026-06-24 11:02:26 +0300

    fix: server

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -35,7 +35,7 @@ export class DatabaseModule {
         imports.push(
           TypeOrmModule.forRoot({
             type: 'postgres',
-            host: 'localhost',
+            host: 'postgres',
             port: 5432,
             username: 'prac',
             password: 'prac',
