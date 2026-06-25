30053f7fb5a8b48d209cadc3df6dba3df2b465a4
Misha Zhuravlev <mishami816@gmail.com>
2026-06-25 11:41:50 +0300

    fix: restart option and remove any types

--- backend/src/logger/tskv.logger.ts
+++ backend/src/logger/tskv.logger.ts
@@ -2,19 +2,19 @@ import { LoggerService, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TskvLogger implements LoggerService {
-  formatMessage(level: string, message: any, ...optionalParams: any[]) {
+  formatMessage(level: string, message: string, ...optionalParams: unknown[]) {
     return `level=${level}\tmessage=${message}\toptionalParams=${optionalParams}`;
   }
 
-  log(message: any, ...optionalParams: any[]) {
+  log(message: string, ...optionalParams: unknown[]) {
     console.log(this.formatMessage('log', message, optionalParams));
   }
 
-  error(message: any, ...optionalParams: any[]) {
+  error(message: string, ...optionalParams: unknown[]) {
     console.error(this.formatMessage('error', message, optionalParams));
   }
 
-  warn(message: any, ...optionalParams: any[]) {
+  warn(message: string, ...optionalParams: unknown[]) {
     console.warn(this.formatMessage('warn', message, optionalParams));
   }
 }
